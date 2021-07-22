67584db851da3cac00e75d22c3e65d8b21659ff6
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-21 23:34:52 -0300

    feat: add services for abstraction

--- application/product_service.go
+++ application/product_service.go
@@ -1,1 +1,20 @@
 package application
+
+import "errors"
+
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (s *ProductService) Get(id string) (ProductInterface, error ) {
+
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+
+	product, err := s.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,30 @@
-package application
+package application_test
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/tonnytg/waffle/application"
+	mock_application "github.com/tonnytg/waffle/application/mocks"
+	"testing"
+)
+
+func TestProductService_Get(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	if err != nil {
+		t.Error(err)
+	}
+	if result != product {
+		t.Error("result must be equal to product")
+	}
+}

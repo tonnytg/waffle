7dc3efd4aa30fb1f977cd560be0a74cab905337e
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-28 22:57:24 -0300

    feat: add test unity for convert erro to json

--- adapters/web/handler/error_json_test.go
+++ adapters/web/handler/error_json_test.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHandler_jsonError(t *testing.T) {
+	msg := "Hello World"
+	result := jsonError(msg)
+	expected := []byte(`{"message":"Hello World"}`)
+
+	if !bytes.Equal(result, expected) {
+		t.Error("result must been equal to expected")
+	}
+}

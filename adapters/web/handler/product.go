c3e221cb830ab53049da03314c4e6feba263c06a
Antonio Thomacelli <tonnytg@gmail.com>
2021-07-29 23:35:25 -0300

    fix: add db connection

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -1,25 +1,37 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/tonnytg/waffle/adapters/db"
 	"github.com/tonnytg/waffle/application"
+	"log"
 	"net/http"
 )
 
 func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
 	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
-		)).Methods("GET", "OPTIONS")
+	)).Methods("GET", "OPTIONS")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		conn, err := sql.Open(db.DatabaseDriver, db.DataSourceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		productDbAdapter := db.NewProductDb(conn)
+		productService := application.NewProductService(productDbAdapter)
+
+
 		w.Header().Set("Content-Type", "application/json")
 		vars := mux.Vars(r)
 		id := vars["id"]
-		product, err := service.Get(id)
+		product, err := productService.Get(id)
 		if err != nil {
 			w.WriteHeader(http.StatusNotFound)
 			return

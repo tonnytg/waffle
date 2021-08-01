df2f7f92b13dbce22da55802e4bb1d82f61fb67a
Antonio Thomacelli <tonnytg@gmail.com>
2021-08-01 08:44:16 -0300

    feat: create method post accept json

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -6,6 +6,7 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/tonnytg/waffle/adapters/db"
+	"github.com/tonnytg/waffle/adapters/dto"
 	"github.com/tonnytg/waffle/application"
 	"log"
 	"net/http"
@@ -15,6 +16,42 @@ func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.
 	r.Handle("/product/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
 	)).Methods("GET", "OPTIONS")
+	r.Handle("/product", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods("POST", "OPTIONS")
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		conn, err := sql.Open(db.DatabaseDriver, db.DataSourceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		productDbAdapter := db.NewProductDb(conn)
+		productService := application.NewProductService(productDbAdapter)
+
+		w.Header().Set("Content-Type", "application/json")
+		var productDto dto.Product
+		err = json.NewDecoder(r.Body).Decode(&productDto)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		product, err := productService.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+	})
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
